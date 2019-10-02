09b8b642b237419b286e80f598b48f2eeb21fef4
AlexV <asmodehn@gmail.com>
2019-10-02 16:55:44 +0200

    first tentative to implement order model

---
+++ aiokraken/model/order.py
@@ -0,0 +1,148 @@
+
+
+class Order:
+    def __init__(self,  pair, volume, leverage = None, relative_starttm = 0, relative_expiretm = 0, fee_currency_base=True, market_price_protection=True, userref = None, execute=False, close= None):
+
+        self.pair = pair
+        self.volume = volume
+        self.leverage = leverage
+
+        self.starttm = '+' + str(relative_starttm) if relative_starttm > 0 else '0'
+        self.expiretm = '+' + str(relative_expiretm) if relative_expiretm > 0 else '0'
+
+        self.oflags = [
+            'fcib' if fee_currency_base else 'fciq'
+        ]
+        if not market_price_protection:
+            self.oflags.append('nompp')
+
+        self.validate = not execute
+        self.userref = userref
+
+        self.close = close
+
+    def __repr__(self):
+        # TODO : find better / cleaner way ?
+        return f"Order: pair: {self.pair} volume: {self.volume} close: {repr(self.close)}"
+
+
+
+class MarketOrder(Order):
+
+    def __init__(self, **kwargs):
+        super(MarketOrder, self).__init__(**kwargs)
+
+
+class LimitOrder(Order):
+
+    def __init__(self, limit_price, **kwargs):
+        self.price = limit_price
+        super(LimitOrder, self).__init__(**kwargs)
+
+
+class StopLossOrder(Order):
+
+    def __init__(self, stop_loss_price, **kwargs):
+        self.price = stop_loss_price
+        super(StopLossOrder, self).__init__(**kwargs)
+
+
+class TakeProfitOrder(Order):
+
+    def __init__(self, take_profit_price, **kwargs):
+        self.price = take_profit_price
+        super(TakeProfitOrder, self).__init__(**kwargs)
+
+
+class StopLossProfitOrder(Order):
+
+    def __init__(self, stop_loss_price, take_profit_price, **kwargs):
+        self.price = stop_loss_price
+        self.price2 = take_profit_price
+        super(StopLossProfitOrder, self).__init__(**kwargs)
+
+
+class StopLossProfitLimitOrder(Order):
+
+    def __init__(self, stop_loss_price, take_profit_price, **kwargs):
+        self.price = stop_loss_price
+        self.price2 = take_profit_price
+        super(StopLossProfitLimitOrder, self).__init__(**kwargs)
+
+
+class StopLossLimitOrder(Order):
+
+    def __init__(self, stop_loss_trigger_price, triggered_limit_price, **kwargs):
+        self.price = stop_loss_trigger_price
+        self.price2 = triggered_limit_price
+        super(StopLossLimitOrder, self).__init__(**kwargs)
+
+
+class TakeProfitLimit(Order):
+
+    def __init__(self, take_profit_trigger_price, triggered_limit_price, **kwargs):
+        self.price = take_profit_trigger_price
+        self.price2 = triggered_limit_price
+        super(TakeProfitLimit, self).__init__(**kwargs)
+
+
+class TrailingStopOrder(Order):
+
+    def __init__(self, trailing_stop_offset, **kwargs):
+        self.price = trailing_stop_offset
+        super(TrailingStopOrder, self).__init__(**kwargs)
+
+
+class TrailingStopLimit(Order):
+
+    def __init__(self, trailing_stop_offset, triggered_limit_offset, **kwargs):
+        self.price = trailing_stop_offset
+        self.price2 = triggered_limit_offset
+        super(TrailingStopLimit, self).__init__(**kwargs)
+
+
+class StopLossAndLimitOrder(Order):
+    def __init__(self, stop_loss_price, limit_price, **kwargs):
+        self.price = stop_loss_price
+        self.price2 = limit_price
+        super(StopLossAndLimitOrder, self).__init__(**kwargs)
+
+
+class SettlePositionOrder(Order):
+    def __init__(self, **kwargs):
+        super(SettlePositionOrder, self).__init__(**kwargs)
+
+
+
+# Declaring intent via functions
+
+
+def bid(order: Order):
+    order.type = 'buy'
+    return order
+
+buy = bid
+
+def ask(order: Order):
+    order.type = 'sell'
+    return order
+
+sell = ask
+
+
+if __name__ == '__main__':
+
+    order = ask(LimitOrder(
+        pair='XBTEUR',
+        volume='3',
+        limit_price=342342,
+        close=bid(MarketOrder(
+            pair='XBTEUR',
+            volume='1'
+        ))
+    ))
+    print(order)
+
+
+
+
---
+++ aiokraken/model/tests/test_order.py
@@ -0,0 +1,41 @@
+import unittest
+from parameterized import parameterized
+
+from ..order import (Order, MarketOrder, LimitOrder, StopLossOrder, TakeProfitOrder, )## more more more
+
+
+class TestOrder(unittest.TestCase):
+
+    @parameterized.expand([
+        ['MYPAIR', 'Volume'],
+    ])
+    def test_init(self, pair, volume):
+        o = Order(pair = pair, volume=volume, )
+        assert o.pair == pair
+        assert o.volume == volume
+
+
+    # TODO : more more more !
+
+
+class TestMarketOrder(unittest.TestCase):
+
+    @parameterized.expand([
+        ['MYPAIR', 'Volume'],
+    ])
+    def test_init(self, pair, volume):
+        o = MarketOrder(pair=pair, volume=volume, )
+        assert o.pair == pair
+        assert o.volume == volume
+
+
+class TestLimitOrder(unittest.TestCase):
+
+    @parameterized.expand([
+        ['MYPAIR', 'Volume', 'price'],
+    ])
+    def test_init(self, pair, volume, price):
+        o = LimitOrder(pair=pair, volume=volume, limit_price=price )
+        assert o.pair == pair
+        assert o.volume == volume
+        assert o.price == price
