8efb212483c4b035fa6aeaa62996e9cd4040f7b3
AlexV <asmodehn@gmail.com>
2019-10-08 16:35:48 +0200

    adding integration test for limit and stop orders

--- tests/aiokraken/rest/test_addorder.py
+++ tests/aiokraken/rest/test_addorder.py
@@ -2,12 +2,12 @@ import pytest
 
 from aiokraken.rest.api import API, Server
 from aiokraken.rest.client import RestClient
-from aiokraken.model.order import MarketOrder, bid, ask
+from aiokraken.model.order import MarketOrder, LimitOrder, StopLossOrder, bid, ask
 
 
 @pytest.mark.asyncio
 @pytest.mark.vcr(filter_headers=['API-Key', 'API-Sign'])
-async def test_add_buy_order():
+async def test_add_buy_market_order():
     from aiokraken.config import load_api_keyfile
     keystruct = load_api_keyfile()
     rest_kraken = RestClient(server=Server(key=keystruct.get('key'),
@@ -23,7 +23,7 @@ async def test_add_buy_order():
 
 @pytest.mark.asyncio
 @pytest.mark.vcr(filter_headers=['API-Key', 'API-Sign'])
-async def test_add_sell_order():
+async def test_add_sell_market_order():
 
     from aiokraken.config import load_api_keyfile
     keystruct = load_api_keyfile()
@@ -31,6 +31,76 @@ async def test_add_sell_order():
                                            secret=keystruct.get('secret')))
     try:
         response = await rest_kraken.ask(order=ask(MarketOrder(pair='XBTEUR', volume='0.01', )))
+    finally:
+        await rest_kraken.close()
+    print(f'response is {response}')
+
+    assert response
+
+
+@pytest.mark.asyncio
+@pytest.mark.vcr(filter_headers=['API-Key', 'API-Sign'])
+async def test_add_buy_limit_order():
+    from aiokraken.config import load_api_keyfile
+    keystruct = load_api_keyfile()
+    rest_kraken = RestClient(server=Server(key=keystruct.get('key'),
+                                           secret=keystruct.get('secret')))
+    try:
+        # CAREFUL here. Orders should be on 'validate' mode, but still it would be better to get current price asap... TODO
+        response = await rest_kraken.bid(order=bid(LimitOrder(pair='XBTEUR', volume='0.01', limit_price=1234)))
+    finally:
+        await rest_kraken.close()
+    print(f'response is {response}')
+
+    assert response
+
+
+@pytest.mark.asyncio
+@pytest.mark.vcr(filter_headers=['API-Key', 'API-Sign'])
+async def test_add_sell_limit_order():
+
+    from aiokraken.config import load_api_keyfile
+    keystruct = load_api_keyfile()
+    rest_kraken = RestClient(server=Server(key=keystruct.get('key'),
+                                           secret=keystruct.get('secret')))
+    try:
+        # CAREFUL here. Orders should be on 'validate' mode, but still it would be better to get current price asap... TODO
+        response = await rest_kraken.ask(order=ask(LimitOrder(pair='XBTEUR', volume='0.01', limit_price=1234)))
+    finally:
+        await rest_kraken.close()
+    print(f'response is {response}')
+
+    assert response
+
+
+@pytest.mark.asyncio
+@pytest.mark.vcr(filter_headers=['API-Key', 'API-Sign'])
+async def test_add_buy_stop_order():
+    from aiokraken.config import load_api_keyfile
+    keystruct = load_api_keyfile()
+    rest_kraken = RestClient(server=Server(key=keystruct.get('key'),
+                                           secret=keystruct.get('secret')))
+    try:
+        # CAREFUL here. Orders should be on 'validate' mode, but still it would be better to get current price asap... TODO
+        response = await rest_kraken.bid(order=bid(StopLossOrder(pair='XBTEUR', volume='0.01', stop_loss_price=1234)))
+    finally:
+        await rest_kraken.close()
+    print(f'response is {response}')
+
+    assert response
+
+
+@pytest.mark.asyncio
+@pytest.mark.vcr(filter_headers=['API-Key', 'API-Sign'])
+async def test_add_sell_stop_order():
+
+    from aiokraken.config import load_api_keyfile
+    keystruct = load_api_keyfile()
+    rest_kraken = RestClient(server=Server(key=keystruct.get('key'),
+                                           secret=keystruct.get('secret')))
+    try:
+        # CAREFUL here. Orders should be on 'validate' mode, but still it would be better to get current price asap... TODO
+        response = await rest_kraken.ask(order=ask(StopLossOrder(pair='XBTEUR', volume='0.01', stop_loss_price=1234)))
     finally:
         await rest_kraken.close()
     print(f'response is {response}')
