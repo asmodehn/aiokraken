b6d8087b6daa8a960002f980c068c9392a8a2481
AlexV <asmodehn@gmail.com>
2019-10-26 11:38:56 +0200

    now running more tests in CI...

--- aiokraken/rest/schemas/tests/__main__.py
+++ aiokraken/rest/schemas/tests/__main__.py
@@ -8,9 +8,9 @@ uses integrated python tools only.
 
 # import your test modules
 if __package__ is not None:
-    from . import test_time, test_ohlc, test_order, test_payload
+    from . import test_time, test_ticker, test_payload, test_ohlc, test_ktype, test_ktm, test_krequestorder, test_kpair, test_kordertype, test_korderdescr, test_kopenorder, test_kcurrency
 else:
-    import test_time, test_ohlc, test_order, test_payload
+    import test_time, test_ticker, test_payload, test_ohlc, test_ktype, test_ktm, test_krequestorder, test_kpair, test_kordertype, test_korderdescr, test_kopenorder, test_kcurrency
 
 # initialize the test suite
 loader = unittest.TestLoader()
@@ -18,9 +18,17 @@ suite = unittest.TestSuite()
 
 # add tests to the test suite
 suite.addTests(loader.loadTestsFromModule(test_time))
-suite.addTests(loader.loadTestsFromModule(test_ohlc))
-suite.addTests(loader.loadTestsFromModule(test_order))
+suite.addTests(loader.loadTestsFromModule(test_ticker))
 suite.addTests(loader.loadTestsFromModule(test_payload))
+suite.addTests(loader.loadTestsFromModule(test_ohlc))
+suite.addTests(loader.loadTestsFromModule(test_ktype))
+suite.addTests(loader.loadTestsFromModule(test_ktm))
+suite.addTests(loader.loadTestsFromModule(test_krequestorder))
+suite.addTests(loader.loadTestsFromModule(test_kpair))
+suite.addTests(loader.loadTestsFromModule(test_kordertype))
+suite.addTests(loader.loadTestsFromModule(test_korderdescr))
+suite.addTests(loader.loadTestsFromModule(test_kopenorder))
+suite.addTests(loader.loadTestsFromModule(test_kcurrency))
 
 # initialize a runner, pass it your suite and run it
 runner = unittest.TextTestRunner(verbosity=3)
