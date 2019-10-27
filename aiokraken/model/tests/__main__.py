71685e478c5384a65c0ad9572cdb6c1b0b35c6fc
AlexV <asmodehn@gmail.com>
2019-10-27 06:53:38 +0100

    quick fix in main model tests

--- aiokraken/model/tests/__main__.py
+++ aiokraken/model/tests/__main__.py
@@ -8,9 +8,9 @@ uses integrated python tools only.
 
 # import your test modules
 if __package__ is not None:
-    from . import test_ohlc, test_order
+    from . import test_ohlc, test_pair, test_ticker
 else:
-    import test_ohlc, test_order
+    import test_ohlc, test_pair, test_ticker
 
 # initialize the test suite
 loader = unittest.TestLoader()
@@ -18,7 +18,8 @@ suite = unittest.TestSuite()
 
 # add tests to the test suite
 suite.addTests(loader.loadTestsFromModule(test_ohlc))
-suite.addTests(loader.loadTestsFromModule(test_order))
+suite.addTests(loader.loadTestsFromModule(test_pair))
+suite.addTests(loader.loadTestsFromModule(test_ticker))
 
 # initialize a runner, pass it your suite and run it
 runner = unittest.TextTestRunner(verbosity=3)
