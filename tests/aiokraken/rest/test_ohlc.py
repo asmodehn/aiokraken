98d9031f2a6b56acc54fd3a98282d51d2bc8e23c
AlexV <asmodehn@gmail.com>
2019-11-23 17:00:45 +0100

    fixing ticker and ohlc tests after client API changes

--- tests/aiokraken/rest/test_ohlc.py
+++ tests/aiokraken/rest/test_ohlc.py
@@ -12,30 +12,24 @@ from aiokraken.model.ohlc import OHLC
 @pytest.mark.vcr()
 async def test_ohlc_pair_newstr():
     """ get kraken ohlc"""
-    rest_kraken = RestClient(server=Server())
-    try:
-        response = await rest_kraken.ohlc(pair="XBTEUR")
+    async with RestClient(server=Server()) as rest_kraken:
+        ohlc_run = rest_kraken.ohlc(pair="XBTEUR")
+        response = await ohlc_run()
         print(f'response is \n{response.head()}')
 
         assert isinstance(response, OHLC)
-
-    finally:
-        await rest_kraken.close()
-
 
 @pytest.mark.asyncio
 @pytest.mark.vcr()
 async def test_ohlc_pair_oldstr():
     """ get kraken ohlc"""
-    rest_kraken = RestClient(server=Server())
-    try:
-        response = await rest_kraken.ohlc(pair="XXBTZEUR")
+    async with RestClient(server=Server()) as rest_kraken:
+        ohlc_run = rest_kraken.ohlc(pair="XXBTZEUR")
+        response = await ohlc_run()
         print(f'response is \n{response.head()}')
 
         assert isinstance(response, OHLC)
 
-    finally:
-        await rest_kraken.close()
 
 
 if __name__ == '__main__':
--- tests/aiokraken/rest/test_ticker.py
+++ tests/aiokraken/rest/test_ticker.py
@@ -8,19 +8,15 @@ from aiokraken.model.ticker import Ticker
 @pytest.mark.asyncio
 @pytest.mark.vcr()
 async def test_ticker_one():
-    """ get kraken ohlc"""
-    rest_kraken = RestClient(server=Server())
-    try:
-
-        response = await rest_kraken.ticker(pairs=['XBTEUR'])
+    """ get kraken ticker"""
+    async with RestClient(server=Server()) as rest_kraken:
+        ticker_run = rest_kraken.ticker(pairs=['XBTEUR'])
+        response = await ticker_run()
         print(f'response is \n{response}')
         assert len(response) == 1
         pm = "XXBTZEUR"  # TODO : handle conversion problem...
         assert pm in response
         assert isinstance(response.get(pm), Ticker)
-
-    finally:
-        await rest_kraken.close()
 
 
 # TODO : multiple ticker request test...
