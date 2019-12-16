11f89d560393315a323514276b1d3d558c27d46b
AlexV <asmodehn@gmail.com>
2019-12-16 15:59:09 +0100

    removing tinymodel from assets and balance. giving up on storage implementation for now.

--- aiokraken/assets.py
+++ aiokraken/assets.py
@@ -1,25 +1,11 @@
 import asyncio
 import dataclasses
-import time
-from collections import OrderedDict
 from collections.abc import Mapping
 
-from pprint import pprint
-
-import typing
-
-from aiokraken.tinymodel import TinyModel, TinyMutableModel, mutate
-from tinydb import TinyDB
-
 from aiokraken.config import load_persist
-from aiokraken.utils import get_kraken_logger, get_nonce
-from aiokraken.rest.api import Server, API
+from aiokraken.utils import get_kraken_logger
 from aiokraken.rest.client import RestClient
-
-from aiokraken.rest.schemas.krequestorder import RequestOrder
-from aiokraken.rest.schemas.kopenorder import KOpenOrderModel
-from aiokraken.rest.schemas.korderdescr import KOrderDescrOnePrice, KOrderDescr
-
+from aiokraken.rest.schemas.kasset import AssetSchema
 
 LOGGER = get_kraken_logger(__name__)
 
@@ -32,9 +18,7 @@ class Assets(Mapping):
     def __init__(self, blacklist = None, whitelist=None):
         self._blacklist = blacklist   # NONE black list means everything forbidden. Empty list means EVERYTHING ALLOWED.
         self._whitelist = whitelist   # NONE white list means everything allowed. Empty list means NOTHNG ALLOWED.
-
-        # Model is mutable here because htere is no ambiguity to who is hte master/writer of data (remote)
-        self._model = TinyModel("Assets", persist_db = load_persist())
+        #TODO : a better way ?
 
     async def __call__(self, rest_client=None):
         """
@@ -43,11 +27,7 @@ class Assets(Mapping):
         rest_client = rest_client or RestClient()
         # On retrieval white list determines what data is knowable into the system.
         assets_run = rest_client.assets(assets=self._whitelist) if self._whitelist else rest_client.assets()  # get all
-        assets = await assets_run()
-
-        with mutate(self._model) as mm:
-            for k, v in assets.items():  # Only when master is remote
-                mm[k] = dataclasses.asdict(v)
+        self.impl = await assets_run()
 
         return self
 
@@ -55,22 +35,22 @@ class Assets(Mapping):
 
     def __getitem__(self, key):
         if key not in self._blacklist:  # filtering on local access based on blacklist.
-            return self._model[key]
+            return self.impl[key]
         else:
             raise KeyError(f"{key} is marked as undesired Asset and is not accessible.")
 
     def __iter__(self):
-        return iter(self._model.keys())
+        return iter(self.impl.keys())
 
     def __len__(self):
-        return len(self._model)
+        return len(self.impl)
 
 
 if __name__ == '__main__':
 
     async def assets_retrieve_nosession():
         assets = Assets(blacklist=[])
-        await assets()
+        await assets()  # Optional from the moment we have Local storage
         for k, p in assets.items():
             print(f" - {k}: {p}")
 
--- aiokraken/balance.py
+++ aiokraken/balance.py
@@ -1,6 +1,6 @@
 import asyncio
-from pprint import pprint
 
+from aiokraken.config import load_account_persist
 from aiokraken.rest import RestClient, Server
 
 from collections.abc import Mapping
@@ -8,39 +8,35 @@ from collections.abc import Mapping
 
 class Balance(Mapping):
 
-    def __init__(self, assets = None):  # refresh period as none means never.
-        self._visible_assets = assets  # None means all
-        self.balance = {}  # TODO : make immutable
+    def __init__(self, blacklist=None, whitelist = None):
+        self._blacklist = blacklist   # NONE black list means everything forbidden. Empty list means EVERYTHING ALLOWED.
+        self._whitelist = whitelist   # NONE white list means everything allowed. Empty list means NOTHNG ALLOWED.
+        #TODO : a better way ?
 
     async def __call__(self, rest_client):
         """
         """
         rest_client = rest_client or RestClient()
-        balance_run = rest_client.balance()  # TODO pass visible_assets
-        accounts = (await balance_run()).accounts  # TODO : why the extra 'accounts' level ? can we get rid of it somehow ?
-
-        self.balance = {k: v for k, v in accounts.items() if self._visible_assets is None or k in self._visible_assets}
+        balance_run = rest_client.balance()  # TODO filter with whitelist
+        self.impl = (await balance_run()).accounts  # TODO : why the extra 'accounts' level ? can we get rid of it somehow ?
 
         return self
 
     # TODO : howto make display to string / repr ??
 
     def __getitem__(self, key):
-        if self.balance is None:
-            raise KeyError
-        return self.balance[key]
+        if key not in self._blacklist:  # filtering on local access based on blacklist.
+            return self.impl[key]
+        else:
+            raise KeyError(f"{key} is marked as undesired Asset and is not accessible.")
 
-    # TODO : maybe we should keep iter design for time dependent collections
-    #     And have key addressing only for time independent collections...
+    # TODO : maybe we should keep iter design for time dependent collections (OHLC, timeseries, etc.)
+    #     And have key addressing only for time independent collections (index is obvious for humans - not a cryptic timestamp)...
     def __iter__(self):
-        if self.balance is None:
-            raise StopIteration
-        return iter(self.balance)
+        return iter(self.impl.keys())
 
     def __len__(self):
-        if self.balance is None:
-            return 0
-        return len(self.balance)
+        return len(self.impl)
 
 
 if __name__ == '__main__':
@@ -50,7 +46,7 @@ if __name__ == '__main__':
         keystruct = load_api_keyfile()
         rest = RestClient(server=Server(key=keystruct.get('key'),
                                              secret=keystruct.get('secret')))
-        balance = Balance()
+        balance = Balance(blacklist=[])
         await balance(rest_client=rest)
         for k, p in balance.items():
             print(f" - {k}: {p}")
