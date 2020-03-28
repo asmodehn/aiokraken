7d573e3d95e2b502b92f9129ea8f52f729c471c4
AlexV <asmodehn@gmail.com>
2020-03-28 18:09:33 +0100

    fixed bokeh gui for rest "onetime" data. still having issues with dynamic websocket update... some dependencies and doc updates for devs

--- DEVELOP.md
+++ DEVELOP.md
@@ -19,11 +19,29 @@ We also advise to setup direnv for seamless interaction with your shell.
 
 # Jupyter & visual tests
 
+Jupyter lab is probably a good solution, but in the mean time we sitkc with a specific way to manipulate notebooks.
+
 We rely on Jupyter and Jupytext (to only have one .py file to care about) for quick visual interactivity.
 
-Make sure you install a kernel with the current virtual environment.
+Juyter and extensions should be installed in your user virtualenv (with --user).
+No point having them in the application venv, and jupyter configuration is per user anyway.
+You then need to start the Jupyter notebook server with the the root notebook directory as argument:
 
-From inside your virtualenv:
+    jupyter notebook Projects/aiokraken
+
+This is necessary so that:
+- your virtual env for the app remains isolated in a kernel
+- the jupyter server has access to all extension modules
+
+We advise installing contrib extensions as well from https://jupyter-contrib-nbextensions.readthedocs.io/en/latest/install.html
+Install hte extension configurator for better usage:
+    
+    pip install jupyter_nbextensions_configurator --user
+    jupyter nbextensions_configurator enable --user
+
+To make the application venv available from jupyter, you need to install a kernel with the current virtual environment.
+
+From inside your application virtualenv:
 
     pipenv install ipykernel
     python -m ipykernel install --user --name=$(basename $VIRTUAL_ENV)
--- bokeh_gui.py
+++ bokeh_gui.py
@@ -55,22 +55,86 @@ output_notebook()
 TOOLS = "pan,wheel_zoom,box_zoom,reset,save"
 # -
 
-p = figure(plot_height=240, tools=TOOLS, x_axis_type="datetime", y_axis_location="right")
+p = figure(plot_height=320, tools=TOOLS, x_axis_type="datetime", y_axis_location="right", sizing_mode="scale_width")
 # p.x_range.follow = "end"
 # p.x_range.follow_interval = 100
 # p.x_range.range_padding = 0
 
 # +
 
-#p.line(x='datetime', y='vwap', line_width=1, color='navy', source=source)
-
 p.segment(x0='datetime', y0='low', x1='datetime', y1='high', line_width=1, color='black', source=source)
 
 inc = source.open < source.close
-dec = source.close > source.open
+dec = source.close < source.open
 w = source.index[1] - source.index[0]  # width is one index step
 
 p.vbar(x=source.index[inc],width=w, bottom=source.open[inc], top=source.close[inc], fill_color="#D5E1DD", line_color="black")
 p.vbar(x=source.index[dec],width=w, top=source.open[dec], bottom=source.close[dec], fill_color="#F2583E", line_color="black")
 
+p.line(x='datetime', y='vwap', line_width=2, color='navy', source=source)
+
 show(p)
+
+# +
+from aiokraken.websockets.client import WssClient
+
+import asyncio
+
+ws = WssClient(restclient=client)
+
+print(ws.loop)
+
+def ws_callback(update):
+    print(update)
+
+await ws.ohlc(["XBTEUR"], callback=ws_callback)
+
+
+async def count(secs):
+    for i in range(1,secs):
+        #to help debug loop/display problems
+        #print('.', end='', flush=True)
+        await asyncio.sleep(1)
+
+await count(10)
+
+
+# +
+p = figure(plot_height=320, tools=TOOLS, x_axis_type="datetime", y_axis_location="right", sizing_mode="scale_width")
+# extra instructions to follow the end of the x range
+p.x_range.follow = "end"
+# p.x_range.follow_interval = 100
+p.x_range.range_padding = 0
+
+# use a ColumnDataSource to get the stream method (on top of existing panda dataframe)
+source = ColumnDataSource(source)
+print(source)
+
+set_trace()
+
+#draw the same plot as before
+p.segment(x0='datetime', y0='low', x1='datetime', y1='high', line_width=1, color='black', source=source)
+
+inc = source.open < source.close
+dec = source.close < source.open
+w = source.index[1] - source.index[0]  # width is one index step
+
+p.vbar(x=source.index[inc],width=w, bottom=source.open[inc], top=source.close[inc], fill_color="#D5E1DD", line_color="black")
+p.vbar(x=source.index[dec],width=w, top=source.open[dec], bottom=source.close[dec], fill_color="#F2583E", line_color="black")
+
+p.line(x='datetime', y='vwap', line_width=2, color='navy', source=source)
+
+show(p)
+
+
+
+def ws_callback(update):
+    print(update)
+    source.stream(update)
+
+    
+await ws.ohlc(["XBTEUR"], callback=ws_callback)
+
+
+await count(10)
+
